821b3cf5e34f4d3137173f7bf95b84b4cec8223f
paul <imsbt@outlook.com>
2023-07-17 19:44:55 +0800

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path')
+const os = require('os')
 const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+const threads = os.cpus().length
 
 function getStyleLoader (pre){
     return [
@@ -39,42 +43,62 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: getStyleLoader(),
-            },
-            // {
-            //     test: /\.less$/,
-            //     use: getStyleLoader('less-loader'),
-            // },
-            // {
-            //     test: /\.s[ac]ss$/,
-            //     use: getStyleLoader('sass-loader'),
-            // },
-            {
-                test: /\.(png|jpe?g|gif|webp|svg)$/,
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 8 * 1024, // 8kb
+                oneOf: [
+                    {
+                        test: /\.css$/,
+                        use: getStyleLoader(),
                     },
-                },
-                generator: {
-                    filename: 'static/images/[hash:10][ext][query]',
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                // options: {
-                //     presets: ['@babel/preset-env'],
-                // },
+                    // {
+                    //     test: /\.less$/,
+                    //     use: getStyleLoader('less-loader'),
+                    // },
+                    // {
+                    //     test: /\.s[ac]ss$/,
+                    //     use: getStyleLoader('sass-loader'),
+                    // },
+                    {
+                        test: /\.(png|jpe?g|gif|webp|svg)$/,
+                        type: 'asset',
+                        parser: {
+                            dataUrlCondition: {
+                                maxSize: 8 * 1024, // 8kb
+                            },
+                        },
+                        generator: {
+                            filename: 'static/images/[hash:10][ext][query]',
+                        }
+                    },
+                    {
+                        test: /\.m?js$/,
+                        exclude: /node_modules/,
+                        use: [
+                            {
+                                loader: "thread-loader",
+                                options: {
+                                    workers: threads,
+                                },
+                            },
+                            {
+                                loader: 'babel-loader',
+                                options: {
+                                    // presets: ['@babel/preset-env'],
+                                    cacheDirectory: true,
+                                    cacheCompression: false,
+                                },
+                            }
+                        ],
+                    }
+                ]
             }
         ],
     },
     plugins: [
         new ESLintPlugin({
             context: path.resolve(__dirname, 'src'),
+            exclude: "node_modules",
+            cache: true,
+            // cacheLocation: path.resolve(__dirname, "node_modules/.cache/eslintcache")
+            threads: threads,
         }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
@@ -82,8 +106,16 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "css/[name].[chunkhash:10].css"
         }),
-        new CssMinimizerPlugin(),
     ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin({
+                parallel: threads,
+            }),
+        ],
+    },
     mode: 'development',
     devServer: {
         port: 8088,
